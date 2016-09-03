cc3a93cbbaccaf5145544d37bf16fb038e760fe9
wyj-suifeng <1041457754@qq.com>
2016-09-03 16:01:06 +0800

    commit

--- app/src/main/java/com/example/lenovo/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/lenovo/listviewapp/MainActivity.java
@@ -17,30 +17,30 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private final static String PRODUCT="product";
-    private final static String PRICE="price";
-    private final static String CONFIGURATION="configuration";
+    private final static String NAME = "n";
+    private final static String GRA = "g";
+    private final static String NUM = "m";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] products={"小米Note","华为荣耀7","魅族MX5","锤子T1"};
-        String[] prices={"1999","1999","1999","2480"};
-        String[] configurations={"高通骁龙 801，3GB RAM，16GB ROM","麒麟 935，3GB RAM，16GB ROM","联发科（MTK)Helio X10 Turbo，3GB RAM，32GB ROM","高通骁龙 801，2GB RAM，32GB ROM"};
+        String[] name = {"ww", "yy", "qq", "hh"};
+        String[] grade = {" 1班 ", " 2班 ", " 3班 ", " 4班 "};
+        String[] num = {"0000", "0001", "0002", "0003"};
 
         List<Map<String,Object>> items=new ArrayList<Map<String,Object>>();
 
-        for(int i=0;i<products.length;i++) {
+        for (int i = 0; i < name.length; i++) {
             Map<String,Object> item=new HashMap<String,Object>();
-            item.put(PRODUCT, products[i]);
-            item.put(PRICE, prices[i]);
-            item.put(CONFIGURATION, configurations[i]);
+            item.put(NAME, name[i]);
+            item.put(GRA, grade[i]);
+            item.put(NUM, num[i]);
             items.add(item);
         }
 
-        SimpleAdapter adapter=new SimpleAdapter(this,items,R.layout.item,new String[]{PRODUCT,PRICE,CONFIGURATION},new int[]{R.id.txtProduct,R.id.txtPrice,R.id.txtConfiguration});
+        SimpleAdapter adapter = new SimpleAdapter(this, items, R.layout.item, new String[]{NAME, GRA, NUM}, new int[]{R.id.txtProduct, R.id.txtPrice, R.id.txtConfiguration});
 
         ListView list=(ListView)findViewById(R.id.list);
 
